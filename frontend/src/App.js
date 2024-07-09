1c08503dc06810681bb4e980d33d2b2f9beb04ab
siso <116954896+sisology@users.noreply.github.com>
2024-07-09 19:17:05 +0900

    front set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <App/>} />
+        <Route path="/" element={ <Main/>} />
       </Routes>
     </BrowserRouter>
   );
