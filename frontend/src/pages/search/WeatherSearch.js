791f5bfe4896029f26341bcd317a8340a9a58cb8
siso <116954896+sisology@users.noreply.github.com>
2024-07-10 18:06:01 +0900

    feat: weather card component

--- frontend/src/pages/search/WeatherSearch.js
+++ frontend/src/pages/search/WeatherSearch.js
@@ -1,8 +1,27 @@
+import OptionBadge from "custom-components/btn/OptionBadge";
+import SearchBtn from "custom-components/btn/SearchBtn";
+import WeatherCard from "custom-components/card/WeatherCard";
+import { Col, Container, Row } from "react-bootstrap";
+
 function WeatherSearch() {
 
     return(
         <>
             <h1>content</h1>
+            <SearchBtn
+                buttonTitle={"풍경 업로드하기"}
+                // onClick={}
+                />
+            <Row className="px-5">
+                {/* 값을 넘길 때는 noValue를 따로 쓰지 않는다. */}
+                <WeatherCard weatherName={"Cloudy"} noValue={true} />
+                <WeatherCard weatherName={"Rainy"} noValue={true} />
+                <WeatherCard weatherName={"Sunny"} noValue={true} />
+                <WeatherCard weatherName={"Snowy"} noValue={true} />
+                <WeatherCard weatherName={"Foggy"} noValue={true} />
+            </Row>
+            <WeatherCard weatherName={"Foggy"}/>
+            <OptionBadge optionName={"test"} />
         </>
     );
 }
