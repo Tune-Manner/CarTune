be9a1b45eaaeba35a8f8f8321d6d23cdfee98d03
siso <116954896+sisology@users.noreply.github.com>
2024-07-10 18:06:27 +0900

    feat: email input component

---
+++ frontend/src/custom-components/form/EmailInput.js
@@ -0,0 +1,23 @@
+import { Button } from "components/ui/button";
+import { Input } from "components/ui/input";
+ 
+function EmailInput() {
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input
+        type="email"
+        placeholder="Email"
+        style={{'borderRadius':'5px'}}
+      />
+      <Button
+        type="submit"
+        variant="outline"
+        style={{'borderRadius':'5px'}}
+      >
+        확인
+      </Button>
+    </div>
+  )
+}
+
+export default EmailInput;
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,11 @@
+import EmailInput from "custom-components/form/EmailInput";
+
+
 function Main() {
     return(
         <>
             <h1>test</h1>
+            <EmailInput/>
         </>
     );
 }
