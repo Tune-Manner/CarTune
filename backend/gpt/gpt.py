fc6bf841b3ea7898e964b0a7ec0ef2264c2db971
universe <hello.its.universe@gmail.com>
2024-07-11 16:34:28 +0900

    gptPrompt 수정

--- backend/gpt/gpt.py
+++ backend/gpt/gpt.py
@@ -12,8 +12,21 @@ cipher_suite = Fernet(key)
 
 openai.api_key = cipher_suite.decrypt(Encrypted_text2).decode()
 
-def gptPrompt(prompt):
+def gptPrompt(weather: str):
     try:
+        prompt = "{} 날씨에 맞는 음악 10개를 추천해줘. 반드시 영어로 아래 양식에 맞게 작성해줘.\n\
+        {{\
+        \t\"songs\": [\
+        \t\t{{ \"title\": \"Spring Day\", \"artist\": \"BTS\" }},\
+        \t\t{{ \"title\": \"Any Song\", \"artist\": \"Zico\" }},\
+        \t\t{{ \"title\": \"DDU-DU DDU-DU\", \"artist\": \"BLACKPINK\" }},\
+        \t\t{{ \"title\": \"Blueming\", \"artist\": \"IU\" }},\
+        \t\t{{ \"title\": \"Cheer Up\", \"artist\": \"TWICE\" }},\
+        \t\t{{ \"title\": \"LILAC\", \"artist\": \"IU\" }}\
+        \t],\
+        \t\"playlist_name\": \"{} day playlist\"\
+        }}".format(weather, weather)
+
         response = openai.chat.completions.create(
             model='gpt-3.5-turbo',
             messages=[
@@ -24,24 +37,13 @@ def gptPrompt(prompt):
             ],
             temperature=0.4
         )
-        print('\nChatGPT is now generating answer ...')
+        # print('\nChatGPT is now generating answer ...')
+
     except Exception as e:
         print("OpenAI API Error!")
         print(f"에러 내용: \n{e}")
         return
     return response.choices[0].message.content
 
-prompt = "비오는 날에 맞는 음악 10개를 추천해줘. 반드시 영어로 아래 양식에 맞게 작성해줘.\n\
-{\
-\t\"songs\": [\
-\t\t{ \"title\": \"Spring Day\", \"artist\": \"BTS\" },\
-\t\t{ \"title\": \"Any Song\", \"artist\": \"Zico\" },\
-\t\t{ \"title\": \"DDU-DU DDU-DU\", \"artist\": \"BLACKPINK\" },\
-\t\t{ \"title\": \"Blueming\", \"artist\": \"IU\" },\
-\t\t{ \"title\": \"Cheer Up\", \"artist\": \"TWICE\" },\
-\t\t{ \"title\": \"LILAC\", \"artist\": \"IU\" }\
-\t],\
-\t\"playlist_name\": \"Rainy day playlist\"\
-}\
-"
-print(gptPrompt(prompt))
+weather = "sunny"
+print(gptPrompt(weather))
