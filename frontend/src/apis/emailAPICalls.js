9d7a72eecffc9cfa5677d2561db7bc72c3fd8e46
siso <116954896+sisology@users.noreply.github.com>
2024-07-12 21:54:50 +0900

    feat: email api

---
+++ frontend/src/apis/emailAPICalls.js
@@ -0,0 +1,24 @@
+import { sendPlaylistSuccess } from "modules/EmailModules";
+import { request } from "./api";
+import axios from "axios";
+
+export const callSendPlaylistAPI = ({ emailData }) => {
+
+  return async (dispatch) => {
+      try {
+          const result = await axios.post('/playlist/send-email', emailData, {
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          });
+          console.log('emailData result : ', result)
+
+          if (result.status === 200) {
+              dispatch(sendPlaylistSuccess());
+          }
+      } catch (error) {
+          console.error('There was an error!', error);
+      }
+  };
+
+};
